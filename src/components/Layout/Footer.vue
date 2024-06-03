33414c6e62960d307ad1c945ef27a0a8e50df1ac
André Paul Grandsire <andrepg@users.noreply.github.com>
2024-06-03 01:43:45 +0000

    ♻️ finish bubble animation

--- src/components/Layout/Footer.vue
+++ src/components/Layout/Footer.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-<footer class="flex flex-col text-xs py-5 px-3 text-white w-full flex justify-center bg-purple-900 shadow-inner shadow-lg mt-4">
+<footer class="flex flex-col text-xs py-5 px-3 text-white w-full justify-center bg-purple-900 shadow-inner z-10 mt-4">
   <span>Made with 🤍 and VueJS</span>
   <span>Hosted by 🤖 GitHub Pages</span>
 
--- src/components/PrimaryButton.vue
+++ src/components/PrimaryButton.vue
@@ -1,6 +1,6 @@
 <script setup>
-import {defineProps} from "vue";
-import {Icon} from "@iconify/vue";
+import { defineProps } from "vue";
+import { Icon } from "@iconify/vue";
 
 const buttonProp = defineProps({
   icon: String,
@@ -10,22 +10,16 @@ const buttonProp = defineProps({
 </script>
 
 <template>
-  <a :href="buttonProp.target"
-     target="_blank"
-     rel="nofollow"
-     class="flex flex-row py-3 px-5 shadow-lg 
-     outline-purple-300 outline outline-1
-     bg-purple-100 text-purple-900 
-     gap-3 justify-start
-        rounded-sm cursor-pointer group
-        transition duration-500 hover:scale-105"
-  >
+  <a :href="buttonProp.target" target="_blank" rel="nofollow" class="
+  flex flex-row py-5 px-6 group gap-3
+  bg-purple-100 hover:bg-purple-800 
+        ">
 
-    <Icon class="text-3xl group-hover:animate-pulse"
-          :icon="buttonProp.icon"/>
+    <Icon class="text-3xl group-hover:animate-pulse text-purple-900 group-hover:text-purple-100"
+      :icon="buttonProp.icon" />
 
-    <span class="w-full text-center text-md">
-      <slot/>
+    <span class="w-fit text-center text-md text-purple-900 group-hover:text-purple-200">
+      <slot />
     </span>
   </a>
 </template>
