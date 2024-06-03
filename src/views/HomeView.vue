b393a1e8ed874b707ca2af48ea9fe45b78afc991
André Paul Grandsire <andrepg@users.noreply.github.com>
2024-06-03 16:03:52 +0000

    ♻️ format homepage content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,41 @@
 <script setup>
-import PrimaryButton from "@/components/PrimaryButton.vue";
-import BubblesAnimation from "@/components/Layout/BubblesAnimation.vue";
-import HomepageLinks from "@/data/homepageLinks.js";
+import PrimaryButton from '@/components/PrimaryButton.vue'
+import BubblesAnimation from '@/components/Layout/BubblesAnimation.vue'
+import HomepageLinks from '@/data/homepageLinks.js'
 </script>
 
 <template>
-  <div id="container" class="flex flex-col gap-5 lg:w-9/12 self-center z-10">
+  <div class="flex flex-col flex-1 md:justify-center gap-5 lg:w-9/12 self-center z-10">
+    <div
+      class="text-purple-900 lg:text-purple-100 w-fit mx-auto py-10 text-center 
+      md:w-3/4 md:text-left md:px-5 md:py-3 md:my-4 md:rounded-lg 
+      lg:bg-purple-800 lg:flex lg:flex-row lg:p-0 lg:my-0 lg:overflow-clip"
+    >
+      <img
+        class="rounded-full align-center mx-auto mb-4 shadow-md h-36 lg:rounded-none
+        lg:mb-0 lg:flex-0"
+        src="https://github.com/andrepg.png"
+        alt="My personal profile photo"
+      />
 
-    <div class="text-purple-900 w-fit mx-auto py-10 text-center 
-        md:w-2/4 md:text-left md:px-5 md:py-3 md:my-4 md:rounded-lg md:bg-purple-100 md:outline md:outline-2 md:outline-purple-100 md:shadow-md">
-        <img
-            class="rounded-full align-center mx-auto mb-4 shadow-lg h-36"
-            src="https://github.com/andrepg.png"
-            alt="My personal profile photo">
-  
-        <h1 class="text-2xl font-bold">👋 Olá, eu sou André Paul</h1>
-        <small class="text-lg leading-none">Posso te ajudar com aplicativos, websites, consultorias e processos.</small>
+      <div class="flex flex-col lg:py-4 lg:px-3 lg:gap-2">
+        <h1 class="text-2xl lg:text-3xl font-bold">👋 Olá, eu sou André Paul</h1>
+        <small class="text-lg leading-none lg:text-lg lg:leading-tight"
+          >Posso te ajudar com aplicativos, websites, consultorias e processos.</small
+        >
+      </div>
     </div>
-  
-    <div class="flex flex-col md:grid md:grid-cols-2 md:w-2/4 gap-0 mx-auto w-72 md:rounded-lg overflow-clip md:shadow-md">
-      <PrimaryButton v-for="button in HomepageLinks"
-                      :key="button.target"
-                      :icon="button.icon"
-                      :target="button.target"
-        >{{ button.label }}</PrimaryButton>
+
+    <div
+      class="flex flex-col md:grid md:grid-cols-2 md:w-3/4 gap-0 mx-auto w-72 md:rounded-lg overflow-clip"
+    >
+      <PrimaryButton
+        v-for="button in HomepageLinks"
+        :key="button.target"
+        :icon="button.icon"
+        :target="button.target"
+        >{{ button.label }}</PrimaryButton
+      >
     </div>
   </div>
 
