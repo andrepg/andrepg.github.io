3792f2cdd6c8d1448a1ba8e45792bf1d03b9ba53
André Paul Grandsire <andrepg@users.noreply.github.com>
2024-06-07 01:28:01 +0000

    ➕ blog: add vite-plugin-markdown and its settings

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,22 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { Mode, plugin as markdown } from "vite-plugin-markdown";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-  ],
+    markdown({
+      mode: [Mode.HTML, Mode.TOC, Mode.VUE]
+    }),
+    // https://github.com/hmsk/vite-plugin-markdown
+  ],  
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-    }
-  }
+      '@blog': fileURLToPath(new URL('./blog', import.meta.url)),
+    },    
+  },  
 })
