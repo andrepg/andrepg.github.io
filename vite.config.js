34efe6b740f14b41ae1623b8c8dfb43646ea2621
André Paul Grandsire <andrepg@users.noreply.github.com>
2024-10-10 15:44:24 -0300

    ➕ include PrismJS as main dependency

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import { Mode, plugin as markdown } from "vite-plugin-markdown";
+import { Mode, plugin as markdown } from 'vite-plugin-markdown'
+import prismjsPlugin from 'vite-plugin-prismjs'
+// import prismjsPlugin from 'vite-plugin-prismjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,12 +15,39 @@ export default defineConfig({
     markdown({
       mode: [Mode.HTML, Mode.TOC, Mode.VUE]
     }),
-    // https://github.com/hmsk/vite-plugin-markdown
-  ],  
+
+    prismjsPlugin({
+      css: true,
+      theme: 'tomorrow',
+      languages: [
+        'docker',
+        'dockerfile',
+        'gitignore',
+        'ignore',
+        'json',
+        'markup',
+        'css',
+        'html',
+        'javascript',
+        'jsx',
+        'tsx',
+        'typescript',
+        'php',
+        'regex',
+        'sql',
+        'dart',
+        'git',
+        'yaml',
+        'bash',
+        'sh',
+        'shell'
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@blog': fileURLToPath(new URL('./blog', import.meta.url)),
-    },    
-  },  
+      '@blog': fileURLToPath(new URL('./blog', import.meta.url))
+    }
+  }
 })
