057a6100cbfe210a67bc1e597de91b0f905e7139
André Paul Grandsire <andrepg@users.noreply.github.com>
2024-10-10 15:24:53 -0300

    ♻️ add Suspend and format App component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup>
-import {RouterView} from 'vue-router'
+import { RouterView } from 'vue-router'
 import MainHeader from "@/components/Layout/MainHeader.vue";
 import Footer from "@/components/Layout/Footer.vue";
 </script>
 
 <template>
-  <MainHeader/>
-  <main class="flex flex-col flex-1">
-    <RouterView />
-  </main>
+  <MainHeader />
+  <Suspense>
+    <main class="flex flex-col flex-1">
+      <RouterView />
+    </main>
+  </Suspense>
   <Footer />
 </template>
-
