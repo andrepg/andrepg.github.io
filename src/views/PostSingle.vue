10552ebeb9141559f89762e43384ae414fe9810f
André Paul Grandsire <andrepg@users.noreply.github.com>
2024-10-10 16:01:31 -0300

    ✨ add default post content

--- src/views/PostSingle.vue
+++ src/views/PostSingle.vue
@@ -1,31 +1,44 @@
 <template>
   <article class="w-full min-h-screen">
-    <div id="pattern" class="mt-3 py-12
+    <section id="pattern" class="mt-3 py-12
           flex flex-col justify-center items-start px-2 sm:px-10
           text-purple-900">
       <h1 class="text-2xl font-semibold flex flex-col md:w-3/4 max-w-[70w]">
-        Slug:  {{ year }} - {{ article }}
+        {{ content.attributes.titulo }}
 
         <small class="text-purple-700 font-normal font-md w-full leading-snug flex-1">
-          <!-- Aqui eu escrevo meus devaneios, registro minhas aventuras de trabalho e compartilho experiências da profissão. -->
+          {{ content.attributes.serie }}
         </small>
       </h1>
-    </div>
 
-    <!-- <section :html="singleArticle.html" /> -->
+      <ul>{{ content.attributes.tags }}</ul>
+      <ul>{{ content.attributes.categories }}</ul>
+    </section>
+
+    <section id="article-body" class="w-full" v-html="content.html"></section>
   </article>
 </template>
 
 <script setup>
+import { nextTick, onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
+
+import Prism from 'prismjs'
 
 const { year, article } = useRoute().params;
 
-// import(`@blog/${year}/${article}.md`)
-// metadata.title = markdown.attributes.titulo;
-// metadata.categories = markdown.attributes.categorias;
-// metadata.tags = markdown.attributes.tags;
-// metadata.serie = markdown.attributes.serie;
-// metadata.content = markdown.html;
+const content = ref({
+  attributes: {},
+  html: '',
+});
 
+onMounted(async () => {
+  Prism.manual = true;
+
+  content.value = await import(`@blog/${year}/${article}.md`);
+
+  nextTick(() => {
+    Prism.highlightAll()
+  })
+})
 </script>
