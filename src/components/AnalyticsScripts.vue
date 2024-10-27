fa6e1d01ecb305a4b909337949cdf79b79ce5a15
André Paul Grandsire <andrepg@users.noreply.github.com>
2024-10-27 17:18:47 -0300

    include tracking only on production

---
+++ src/components/AnalyticsScripts.vue
@@ -0,0 +1,28 @@
+<script>
+function setupTagManager(w, d, s, l, i) {
+  w[l] = w[l] || []; w[l].push({
+    'gtm.start':
+      new Date().getTime(), event: 'gtm.js'
+  }); var f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+      'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+}
+
+function setupMSClarity(c, l, a, r, i, t, y) {
+  c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+  t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+  y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+}
+
+if (import.meta.env.VITE_ENV == 'production') {
+  console.log("Enabling tracking scripts")
+  setupTagManager(window, document, 'script', 'dataLayer', 'GTM-M3XF5L6')
+  setupMSClarity(window, document, 'clarity', 'script', 'mm8qkhz9hd')
+} else {
+  console.log("Tracking scripts disabled.")
+}
+</script>
+
+<template>
+
+</template>
--- src/components/Layout/Footer.vue
+++ src/components/Layout/Footer.vue
@@ -1,15 +1,14 @@
 <script setup>
-
+import AnalyticsScripts from "@/components/AnalyticsScripts.vue"
 </script>
 
 <template>
-<footer class="flex flex-col text-xs py-5 px-3 text-white w-full justify-center bg-purple-900 shadow-inner z-10 mt-4">
-  <span>Made with 🤍 and VueJS</span>
-  <span>Hosted by 🤖 GitHub Pages</span>
+  <footer class="flex flex-col text-xs py-5 px-3 text-white w-full justify-center bg-purple-900 shadow-inner z-10 mt-4">
+    <span>Made with 🤍 and VueJS</span>
+    <span>Hosted by 🤖 GitHub Pages</span>
 
-</footer>
+    <AnalyticsScripts />
+  </footer>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
