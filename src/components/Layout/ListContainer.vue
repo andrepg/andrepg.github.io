24e841fec06d11e7c112ea1cdaddaa8bdbbcf50b
André Paul Grandsire <andrepg@users.noreply.github.com>
2024-11-12 19:32:13 -0300

    create ListContainer component

---
+++ src/components/Layout/ListContainer.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="w-full px-6 max-w-5xl mx-auto flex flex-col flex-wrap gap-5">
+    <slot/>
+  </section>
+</template>
+<script>
+export default {
+  name: 'Container'
+}
+</script>
