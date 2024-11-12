27e2304bedae452a04684df152f1495d9b6461b6
André Paul Grandsire <andrepg@users.noreply.github.com>
2024-11-12 19:32:25 -0300

    create PostList component template

---
+++ src/components/PostListElement.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  post: {
+    type: {
+      path: String,
+      title: String,
+      excerpt: String,
+      category: Array,
+      tags: Array,
+      published_at: String,
+    }
+  }
+})
+</script>
+
+<template>
+  <a :href="'/blog/'.concat(post.path)" class="">
+    <h2>{{ post.title }}</h2>
+    <p>{{ post.excerpt }}</p>
+  </a>
+</template>
