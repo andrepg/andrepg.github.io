cd8ef9ee3e33cb9ee08a6af0ceb1fbbde8a9ad60
André Paul Grandsire <andrepg@users.noreply.github.com>
2026-02-07 17:04:56 -0300

    upgrade to tailwind 4

--- src/components/Layout/BubblesAnimation.vue
+++ src/components/Layout/BubblesAnimation.vue
@@ -1,40 +1,27 @@
 <template>
-  <ul id="bubbles">
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
+  <ul id="bubbles" class="absolute w-full h-full overflow-hidden opacity-30 bottom-0 left-0">
+    <li v-for="bubble in Array.from({ length: 12})"
+      :key="'bubble-' + bubble"
+      class="absolute list-none flex rounded-full! overflow-clip will-change-transform transforom-gpu bg-purple-500 -bottom-1/3 left-0 z-0 m-0 p-0">
+    </li>
   </ul>
 </template>
 
 <style scoped>
-#bubbles {
-  @apply absolute bottom-0 left-0 w-full h-full overflow-hidden z-0 m-0 p-0 opacity-30;
-}
-
 #bubbles li {
-  animation: 20s bubblesUp 0s linear infinite;
-  @apply absolute list-none flex w-5 h-5 bg-purple-500 bottom-[-400px] rounded-full! overflow-clip;
+  animation: 80s bubblesUp 0s linear infinite;
 }
 
 #bubbles li:nth-child(1) { left: 25%; width: 80px; height: 80px; animation-delay: 0s; }
-#bubbles li:nth-child(2) { left: 10%; width: 20px; height: 20px; animation-delay: 2s; animation-duration: 12s; }
+#bubbles li:nth-child(2) { left: 10%; width: 20px; height: 20px; animation-delay: 2s; }
 #bubbles li:nth-child(3) { left: 70%; width: 20px; height: 20px; animation-delay: 4s; }
-#bubbles li:nth-child(4) { left: 40%; width: 60px; height: 60px; animation-delay: 0s; animation-duration: 18s; }
-#bubbles li:nth-child(5) { left: 65%; width: 20px; height: 20px; animation-delay: 0s; }
-#bubbles li:nth-child(6) { left: 75%; width: 110px; height: 110px; animation-delay: 3s; }
-#bubbles li:nth-child(7) { left: 35%; width: 150px; height: 150px; animation-delay: 7s; }
-#bubbles li:nth-child(8) { left: 50%; width: 25px; height: 25px; animation-delay: 15s; animation-duration: 45s; }
-#bubbles li:nth-child(9) { left: 20%; width: 15px; height: 15px; animation-delay: 2s; animation-duration: 35s; }
-#bubbles li:nth-child(10) { left: 85%; width: 150px; height: 150px; animation-delay: 0s; animation-duration: 12s; }
+#bubbles li:nth-child(4) { left: 40%; width: 60px; height: 60px; animation-delay: 6s; }
+#bubbles li:nth-child(5) { left: 65%; width: 20px; height: 20px; animation-delay: 8s; }
+#bubbles li:nth-child(6) { left: 75%; width: 110px; height: 110px; animation-delay: 10s; }
+#bubbles li:nth-child(7) { left: 35%; width: 150px; height: 150px; animation-delay: 12s; }
+#bubbles li:nth-child(8) { left: 50%; width: 25px; height: 25px; animation-delay: 14s; }
+#bubbles li:nth-child(9) { left: 20%; width: 15px; height: 15px; animation-delay: 16s; }
+#bubbles li:nth-child(10) { left: 85%; width: 150px; height: 150px; animation-delay: 0s; }
 
 @keyframes bubblesUp { 
   0% { 
--- src/components/Layout/SectionWithHeader.vue
+++ src/components/Layout/SectionWithHeader.vue
@@ -1,18 +1,5 @@
 <template>
   <section class="relative py-16 flex flex-col justify-center items-start px-2 sm:px-10">
-    <div v-if="showPattern" class="dark:opacity-20 opacity-5 z-0 absolute w-full h-full left-0 top-0" id="pattern" />
-    <div v-if="showPattern" class="bg-linear-to-b from-transparent to-white dark:to-gray-900 to-95% z-10 absolute w-full h-full left-0 top-0" />
-
-    <div class="z-20">
-      <slot />
-    </div>
+    <slot />
   </section>
 </template>
-
-<script setup>
-import { defineProps } from 'vue';
-
-defineProps({
-  showPattern: false
-})
-</script>
