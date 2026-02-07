0d538c22844ac8f1676db17167220cdbac6fcacd
André Paul Grandsire <andrepg@users.noreply.github.com>
2026-02-07 17:29:14 -0300

    improve navbar

--- src/views/partials/NavbarFeature.vue
+++ src/views/partials/NavbarFeature.vue
@@ -1,10 +1,58 @@
+<script setup>
+import { cn } from "@sglara/cn";
+
+import { Icon } from '@iconify/vue';
+import { HomepageMenu } from '@/data/HomepageMenu';
+import { onMounted, ref } from 'vue';
+
+const opaqueNavbar = ref(false);
+
+let title = "";
+
+if (window.location.pathname === "/curriculo") {
+    title = "Currículo";
+}
+
+if (window.location.pathname.includes('blog')) {
+    title = "Blog";
+}
+
+const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+onMounted(() => {
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 100) {
+            opaqueNavbar.value = true;
+        } else {
+            opaqueNavbar.value = false;
+        }
+    });
+})
+</script>
+
 <template>
-    <nav class="navbar z-50 px-4 fixed top-0 w-full">
+    <nav :class="cn('navbar fixed top-0',
+        'z-50',
+        'py-2',
+        'px-4',
+        'w-full',
+        'transition-all',
+        'ease-in-out',
+        'delay-500',
+        opaqueNavbar && 'bg-primary',
+        !opaqueNavbar && 'bg-transparent')">
+
         <div class="flex-none">
             <div class="drawer">
                 <input id="my-drawer" type="checkbox" class="drawer-toggle" />
                 <div class="drawer-content">
-                    <label for="my-drawer" class="drawer-button btn btn-primary btn-soft btn-square">
+                    <label for="my-drawer" class="duration-1000 ease drawer-button btn btn-primary btn-square"
+                        :class="opaqueNavbar ? 'btn-soft' : 'btn-primary'">
                         <Icon icon="mdi:hamburger" class="dark:text-neutral-content size-5" />
                     </label>
                 </div>
@@ -31,35 +79,33 @@
         </div>
 
         <div class="flex-1 px-4">
-            <div class="text-xl">{{ title }}</div>
+            <h1 :class="cn(
+                'w-1/2',
+                'm-0',
+                'text-xl',
+                'font-medium',
+                'transition-all',
+                'duration-300',
+                'delay-200',
+                'ease-in-out',
+                opaqueNavbar ? 'text-primary-content' : 'text-primary',
+                opaqueNavbar ? 'translate-x-0' : 'translate-x-[90%]'
+            )">{{ title }}</h1>
         </div>
 
         <div class="flex-none">
-            <button title="Voltar ao topo" @click="scrollToTop" class="btn btn-secondary btn-soft btn-square">
+            <button title="Voltar ao topo" @click="scrollToTop"
+                :class="cn(
+                    'btn',
+                    'btn-secondary',
+                    'btn-soft',
+                    'btn-square',
+                    'duration-1000 ease transition-all',
+                    'delay-250',
+                    opaqueNavbar ? 'opacity-100' : 'opacity-0'
+                )">
                 <Icon icon="mdi:arrow-up" class="dark:text-neutral-content text-primary size-5" />
             </button>
         </div>
     </nav>
 </template>
-
-<script setup>
-import { Icon } from '@iconify/vue';
-import { HomepageMenu } from '@/data/HomepageMenu';
-
-let title = "";
-
-if (window.location.pathname === "/curriculo") {
-    title = "Currículo";
-}
-
-if (window.location.pathname.includes('blog')) {
-    title = "Blog";
-}
-
-const scrollToTop = () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-}
-</script>
