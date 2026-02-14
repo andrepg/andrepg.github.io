337ef9d0048c261511a8023be3de3f74d425ca8e
André Paul Grandsire <andrepg@users.noreply.github.com>
2026-02-14 20:19:34 -0300

    bootstrap vite on dev & vite-ssg on production

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,39 @@
 import './assets/main.css'
 
+import { createApp as createVueApp } from 'vue'
 import { ViteSSG } from 'vite-ssg'
 
 import App from './App.vue'
 import { HomepageMenu } from '@data/NavigationMenu'
+import { createRouter, createWebHistory } from 'vue-router'
+import { createHead } from '@unhead/vue/client'
 
 // TODO Maybe move this to a config file
 export const BASE_URL = import.meta.env.BASE_URL;
 
-export const createApp = ViteSSG(App, {
+const bootstrapDevelopmentMode = () => {
+    const router = createRouter({
+        history: createWebHistory(),
+        routes: HomepageMenu,
+    })
+
+    createVueApp(App)
+        .use(router)
+        .use(createHead())
+        .mount('#app')
+}
+
+const bootstrapProductionMode = () => ViteSSG(App, {
     routes: HomepageMenu,
     base: BASE_URL
 })
+
+let vueApp;
+
+if (import.meta.env.DEV) {
+    bootstrapDevelopmentMode()
+} else {
+    vueApp = bootstrapProductionMode()
+}
+
+export const createApp = vueApp;
