7e5acfba1eaf795d101c4533ccf3c98498ae1a06
André Paul Grandsire <andrepg@users.noreply.github.com>
2026-08-21 21:19:59 -0300

    refactor: remove unused import

--- src/components/Features/ProfileCardFeature.vue
+++ src/components/Features/ProfileCardFeature.vue
@@ -1,7 +1,5 @@
 <script setup>
 import { UserConfig } from '@data/website'
-
-import GlassCard from '@/components/GlassCard.vue'
 import SocialMediaShortcuts from '@/components/SocialMediaShortcuts.vue'
 import AsyncImage from '@/components/AsyncImage.vue'
 </script>
